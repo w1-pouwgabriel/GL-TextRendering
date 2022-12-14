d0d1cf6e6a3dc86e57ce8b154708c11643078388
Gabriel Pouw <gabriel@pouw.com>
2022-12-14 21:51:13 +0100

    [CHANGED] Gets the size of the text and creates a buffer for the glyps, does not draw

--- GL-TextRendering/src/main.cpp
+++ GL-TextRendering/src/main.cpp
@@ -7,15 +7,16 @@
 
 #include "graphics/shader.h"
 
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
 #include <map>
 #include <vector>
 
-#define STB_TRUETYPE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-
 //Load std
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb/stb_image_write.h>
+
+#define STB_TRUETYPE_IMPLEMENTATION
 #include <stb/stb_truetype.h>
 
 // settings
@@ -29,15 +30,13 @@ void RenderText(Shader& shader, std::string text, float x, float y, float scale,
 
 unsigned int VAO, VBO;
 
-struct Character
+struct Text
 {
-    unsigned int TextureID;  // ID handle of the glyph texture
-    glm::ivec2   Size;       // Size of glyph
-    glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
-    unsigned int Advance;    // Offset to advance to next glyph
+    int width;
+    int height;
+    unsigned char* pixels;
 };
 
-std::map<char, Character> Characters;
 
 int main()
 {
@@ -77,6 +76,7 @@ int main()
     glEnable(GL_CULL_FACE);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
     // build and compile shaders
     // -------------------------
@@ -88,89 +88,79 @@ int main()
 
     // load font and create a bitmap
     // -----------------------------
-    stbtt_fontinfo font;
-    unsigned char fontBuffer[200];
+    long size;
+    unsigned char* fontBuffer;
+    Text text{0, 0};
 
-    FILE* fp;
-    errno_t err;
-    if((err = fopen_s(&fp, "src/resources/fonts/verdana.ttf", "rb")) != 0)
+    FILE* fontFile = fopen("src/resources/fonts/verdana.ttf", "rb");
+    fseek(fontFile, 0, SEEK_END);
+    size = ftell(fontFile); /* how long is the file ? */
+    fseek(fontFile, 0, SEEK_SET); /* reset */
+
+    fontBuffer = static_cast<unsigned char*>(malloc(size));
+
+    fread(fontBuffer, size, 1, fontFile);
+    fclose(fontFile);
+
+    /* prepare font */
+    stbtt_fontinfo fontInfo;
+    if (!stbtt_InitFont(&fontInfo, fontBuffer, 0))
     {
-        std::cout << "Could not open file" << std::endl;
+        printf("failed\n");
     }
-    else
+
+    float scale = stbtt_ScaleForPixelHeight(&fontInfo, 48 * 2);
+
+    int ascent, descent;
+    stbtt_GetFontVMetrics(&fontInfo, &ascent, &descent, 0);
+    int baseline = (int)(ascent * scale);
+
+    const char* content = " text ";
+
+    //Create a empty space we can draw our text to
+    for (int i = 0; i < strlen(content); i++) 
     {
-        // Read the font into a buffer
-        fread(fontBuffer, 1, 128, fp);
-        stbtt_InitFont(&font, fontBuffer, 0);
+        int advance, leftSideBearing;
+        stbtt_GetCodepointHMetrics(&fontInfo, content[i], &advance, &leftSideBearing);
 
-        // Get a font size
-        float scale = stbtt_ScaleForPixelHeight(&font, 48 * 2);
-
-        int ascent, descent;
-        stbtt_GetFontVMetrics(&font, &ascent, &descent, 0);
-        int baseline = (int)(ascent * scale);
-
-
-        int width = 0;
-        for (const auto& character : fontBuffer)
-        {
-            int advance, leftSideBearing;
-            stbtt_GetCodepointHMetrics(&font, character, &advance, &leftSideBearing);
-            width += advance * scale;
-        }
-
-        int height = (ascent + (descent * -1)) * scale;
-
-        std::vector<unsigned char> pixels((size_t)(width * height), (unsigned char)0);
-
-        float xpos = 0.0f;
-        int characterIndex = 0;
-        while (fontBuffer[characterIndex]) {
-            int advance, lsb, x0, y0, x1, y1;
-            float x_shift = xpos - (float)floor(xpos);
-
-            stbtt_GetCodepointHMetrics(
-                &font, fontBuffer[characterIndex], &advance, &lsb);
-
-            stbtt_GetCodepointBitmapBoxSubpixel(
-                &font,
-                fontBuffer[characterIndex],
-                scale,
-                scale,
-                x_shift,
-                0,
-                &x0,
-                &y0,
-                &x1,
-                &y1);
-
-            auto stride = width * (baseline + y0) + (int)xpos + x0;
-            stbtt_MakeCodepointBitmapSubpixel(
-                &font,
-                &pixels.at(0) + stride,
-                x1 - x0,
-                y1 - y0,
-                width,
-                scale,
-                scale,
-                x_shift,
-                0,
-                fontBuffer[characterIndex]);
-
-            xpos += (advance * scale);
-
-            if (fontBuffer[characterIndex + 1]) {
-                int kernAdvance = stbtt_GetCodepointKernAdvance(
-                    &font, fontBuffer[characterIndex], fontBuffer[characterIndex + 1]);
-                xpos += scale * kernAdvance;
-            }
-
-            ++characterIndex;
-        }
-
-        // This step works fine, this means that the data in pixels is good
-        stbi_write_png("image.png", width, height, 1, pixels.data(), 0);
+        text.width += advance * scale;
     }
+
+    text.height = (ascent + (descent * -1)) * scale;
+    text.pixels = static_cast<unsigned char*>(calloc(text.width * text.height, sizeof(unsigned char)));
+
+    int x = 0;
+    int i;
+    for (i = 0; i < strlen(content); ++i)
+    {
+        /* how wide is this character */
+        int ax;
+        int lsb;
+        stbtt_GetCodepointHMetrics(&fontInfo, content[i], &ax, &lsb);
+        /* (Note that each Codepoint call has an alternative Glyph version which caches the work required to lookup the character word[i].) */
+
+        /* get bounding box for character (may be offset to account for chars that dip above or below the line) */
+        int c_x1, c_y1, c_x2, c_y2;
+        stbtt_GetCodepointBitmapBox(&fontInfo, content[i], scale, scale, &c_x1, &c_y1, &c_x2, &c_y2);
+
+        /* compute y (different characters have different heights) */
+        int y = ascent + c_y1;
+
+        /* render character (stride and offset is important here) */
+        int byteOffset = x + roundf(lsb * scale) + (y * text.width);
+        stbtt_MakeCodepointBitmap(&fontInfo, text.pixels + byteOffset, c_x2 - c_x1, c_y2 - c_y1, text.width, scale, scale, content[i]);
+
+        /* advance x */
+        x += roundf(ax * scale);
+
+        /* add kerning */
+        int kern;
+        kern = stbtt_GetCodepointKernAdvance(&fontInfo, content[i], content[i + 1]);
+        x += roundf(kern * scale);
+    }
+
+    // This step works fine, this means that the data in pixels is good
+    stbi_write_png("src/resources/textures/image.png", text.width, text.height, 1, text.pixels, 0);
 
     // configure VAO/VBO for texture quads
     // -----------------------------------
@@ -223,52 +213,6 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-}
-
-// render a piece of text: uses the font atlas to look up the right spacing
-// ------------------------------------------------------------------------
-void RenderText(Shader& s, std::string text, float x, float y, float scale, glm::vec3 color)
-{
-    // activate corresponding render state	
-    s.use();
-    glUniform3f(glGetUniformLocation(s.ID, "textColor"), color.x, color.y, color.z);
-    glActiveTexture(GL_TEXTURE0);
-    glBindVertexArray(VAO);
-
-    // iterate through all characters
-    std::string::const_iterator c;
-    for (c = text.begin(); c != text.end(); c++)
-    {
-        Character ch = Characters[*c];
-
-        float xpos = x + ch.Bearing.x * scale;
-        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
-
-        float w = ch.Size.x * scale;
-        float h = ch.Size.y * scale;
-        // update VBO for each character
-        float vertices[6][4] = {
-            { xpos,     ypos + h,   0.0f, 0.0f },
-            { xpos,     ypos,       0.0f, 1.0f },
-            { xpos + w, ypos,       1.0f, 1.0f },
-
-            { xpos,     ypos + h,   0.0f, 0.0f },
-            { xpos + w, ypos,       1.0f, 1.0f },
-            { xpos + w, ypos + h,   1.0f, 0.0f }
-        };
-        // render glyph texture over quad
-        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-        // update content of VBO memory
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        // render quad
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
-    }
-    glBindVertexArray(0);
-    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
